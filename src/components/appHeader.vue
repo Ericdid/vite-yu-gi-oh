c6ad235c5d6844242f53aa06d11f0e67ec9bc9be
Ericdid <ericdidom@gmail.com>
2023-09-12 20:11:23 +0200

    add.appHeader

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,21 +1,21 @@
 <script>
-import appHeader from "./components/appHeader.vue";
-
 export default {
   data() {
     return {
       title: "Yu-Gi-Ho Api",
     };
   },
-  components: { appHeader },
 };
 </script>
 
 <template>
-  <appHeader />
+  <header class="d-flex align-items-center px-3">
+    <img src="../assets/img/logo.png" alt="" />
+    <h1>{{ title }}</h1>
+  </header>
 </template>
 
-<style>
+<style scoped>
 img {
   width: 80px;
 }
